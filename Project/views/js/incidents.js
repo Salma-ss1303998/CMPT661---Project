44b99adf9b382b47cba0f2186d88066ec06be58f
Salma <ss1303998@qu.edu.qa>
2017-12-25 17:13:58 +0300

    anything

--- Project/views/js/incidents.js
+++ Project/views/js/incidents.js
@@ -1,3 +1,22 @@
+const IncidentsTemplate = `
+    <h4>Selected Student:</h4>
+    <table class="table table-striped">
+        <tbody>
+        <tr>
+            <td>Student ID</td>
+            <td>First Name</td>
+            <td>Last Name</td>
+        </tr>
+         
+        <tr>
+            <td><a href="">{{studentId}}</a></td>
+            <td>{{firstName}}</td>
+            <td>{{lastName}}</td>
+        </tr>
+        </tbody>
+    </table>
+`
+
 const IncidentTemplate = `
     <h4>Selected Student:</h4>
     <table class="table table-striped">
@@ -5,11 +24,15 @@ const IncidentTemplate = `
         <tr>
             <td>Student ID</td>
             <td>{{studentId}}</td>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>DateOfBirth</td>
+            <td>Gender</td>
+            <td>Email</td>
+            <td>Grade</td>
         </tr>
-<<<<<<< HEAD
          
 =======
->>>>>>> a8fe86c13bdb7d96d989fa5ef3a5a9558fa0b072
         <tr>
             <td>First Name</td>
             <td>{{firstName}}</td>
@@ -17,6 +40,10 @@ const IncidentTemplate = `
         <tr>
             <td>Last Name</td>
             <td>{{lastName}}</td>
+            <td>{{dateOfBirth}}</td>
+            <td>{{gender}}</td>
+            <td>{{email}}</td>
+            <td>{{grade}}</td> 
         </tr>
         </tbody>
 
@@ -27,6 +54,10 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('#studentsList').addEventListener("change", onChange)
 })
 
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("js-DOM fully loaded and parsed");
+    document.querySelector('#').addEventListener("click",onClick)
+})
 async function getStudent(studentID) {
     const url = `/api/students/${studentID}`
     const response = await fetch(url)
@@ -46,7 +77,7 @@ async function onChange(e) {
     try {
         const student = await getStudent(selectedStudentId)
         console.log(student);
-        const htmlTemplate = Handlebars.compile(IncidentTemplate)
+        const htmlTemplate = Handlebars.compile(IncidentsTemplate)
         const htmlContent = htmlTemplate(student)
 
         document.querySelector('#incidentsDetails').innerHTML = htmlContent
@@ -55,3 +86,25 @@ async function onChange(e) {
         console.log(err)
     }
 }
+async function onClick(e) {
+    const selectedStudentId = this.value;
+
+    if (selectedStudentId == "") {
+        document.querySelector('#incidentsDetails').innerHTML = '';
+        return
+    }
+    console.log("onStudentChange.selectedStudentId:", selectedStudentId)
+
+    try {
+        const student = await getStudent(selectedStudentId)
+        console.log(student);
+        const htmlTemplate = Handlebars.compile(IncidentsTemplate)
+        const htmlContent = htmlTemplate(student)
+
+        document.querySelector('#incidentsDetails').innerHTML = htmlContent
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
